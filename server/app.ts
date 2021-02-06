e60432120deb7461b3547a221a9e722545d5838d
Rose Hirigoyen <rosehirigoyen@gmail.com>
2021-02-05 21:19:19 -0500

    server no db

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,38 @@
-import express from 'express';
-import * as bodyParser from 'body-parser';
+import express from "express";
+import * as bodyParser from "body-parser";
+import { getAllRestaurants, getRestaurantInfo, postReservation } from "./userRoutes";
+import {
+  getTodayStats,
+  getPredictedStats,
+  login,
+  postRestaurantSetup,
+  postNewRecipe,
+  postNewWalkInClient,
+} from "./ownerRoutes";
+
 const app = express();
 
-app.use(bodyParser.json({
-    limit: '50mb',
+app.use(
+  bodyParser.json({
+    limit: "50mb",
     verify(req: any, res, buf, encoding) {
-        req.rawBody = buf;
-    }
-}));
+      req.rawBody = buf;
+    },
+  })
+);
 
-app.get('/', (req, res) => res.send('Hello World!'));
-export {app};
+app.get("/", (_, res) => res.send("Nestorant Server"));
+
+// CLIENT ROUTES
+app.get("/client/restaurants/", (req, res) => getAllRestaurants(req, res));
+app.get("/client/restaurants/:restaurantID", (req, res) => getRestaurantInfo(req, res));
+app.post("/client/reservation/", (req, res) => postReservation(req, res));
+
+// RESTAURATEUR ROUTES
+app.get("/owner/:restaurantID/today/", (req, res) => getTodayStats(req, res));
+app.get("/owner/:restaurantID/predicted/", (req, res) => getPredictedStats(req, res));
+app.post("/owner/login/", (req, res) => login(req, res));
+app.post("/owner/:restaurantID/setup/", (req, res) => postRestaurantSetup(req, res));
+app.post("/owner/:restaurantID/recipe/", (req, res) => postNewRecipe(req, res));
+app.post("/owner/:restaurantID/walkin/", (req, res) => postNewWalkInClient(req, res));
+export { app };
---
+++ server/ownerRoutes.ts
@@ -0,0 +1,25 @@
+export function getTodayStats(req: any, res: any) {
+    res.send("TODO")
+}
+
+export function getPredictedStats(req: any, res: any) {
+    res.send("TODO")
+}
+
+export function login(req: any, res: any) {
+    res.send("TODO")
+}
+
+export function postRestaurantSetup(req: any, res: any) {
+    res.send("TODO")
+}
+
+export function postNewRecipe(req: any, res: any) {
+    res.send("TODO")
+}
+
+export function postNewWalkInClient(req: any, res: any) {
+    res.send("TODO")
+}
+
+
